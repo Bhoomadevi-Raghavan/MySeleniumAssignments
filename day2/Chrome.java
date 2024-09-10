e7840b7015908a2e0a1b57f378cadfa8440d857c
Bhoomadevi-Raghavan <bhooms.balaji@gmail.com>
2024-09-10 18:51:43 +0530

    Add files via upload
    Day 1 and Day2 assignments

---
+++ day2/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version =9.1f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+		System.out.println("Version: "+version+"\nDeveloper : "+developer+"\nIsBeta: "+isBeta+"\nRelease Year: "+releaseYear+"\nShortcut Key: "+shortcutKey);
+		
+
+	}
+
+}
---
+++ day2/FireFox.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		
+		float browserVersion=100.2f;
+		String browserName = "firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		
+		System.out.println("Browser Version: " + browserVersion + "\nBrowser Name: " + browserName + "\nIs browser visible: " +isVisible +"\nRelease Year: "+releaseYear+"\nBrowser Logo: " +browserLogo);
+	 
+		
+
+	}
+
+}
